414917908103c20e9095853af48d6fa8654186df
ridharahmi <ridha.rahmi13@gmail.com>
2021-12-03 12:17:07 +0100

    update header

--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,8 @@
 <header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <!-- Header -->
         <div class="container">
-            <a class="navbar-brand" href="index.php">Bootstrap Sass</a>
+            <a class="navbar-brand" href="index.php"><span class="bootstrap">Bootstrap</span> <span class="sass">Sass</span></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
